c511eeb958f23b7b96cab1c9aa41f28747828129
grossmanlev <lgrossman@college.harvard.edu>
2019-04-03 20:56:17 -0400

    spacing stuff

--- agents.py
+++ agents.py
@@ -4,32 +4,35 @@ from torch.distributions.normal import Normal
 from torch.distributions.multivariate_normal import MultivariateNormal as MVN
 import torch.nn.functional as F
 
-class Central():
-	def __init__(self,model,optim):
-		self.model = model
-		self.optim = optim
 
-	def update_model(self,ups): # ups is a tuple consisting of weight and bias grads
-		self.model.weight.zero_grad()
-		self.model.weight.grad = ups[0]
-		self.model.bias.grad = ups[1]
-		self.optim.step()
-		self.model.weight.zero_grad()
+class Central():
+    def __init__(self, model, optim):
+        self.model = model
+        self.optim = optim
+
+    def update_model(self, ups): # ups is a tuple consisting of weight and bias grads
+        self.model.weight.zero_grad()
+        self.model.weight.grad = ups[0]
+        self.model.bias.grad = ups[1]
+        self.optim.step()
+        self.model.weight.zero_grad()
+
 
 class Worker():
-	def __init__(self,loss):
-		self.model = None
-		self.loss = loss
+    def __init__(self, loss):
+        self.model = None
+        self.loss = loss
 
-	def fwd_bkwd(self,inp,outp):
-		self.model.weight.zero_grad()
+    def fwd_bkwd(self, inp, outp):
+        self.model.weight.zero_grad()
 
-		pred = self.model(inp)
-		lossval = self.loss(pred,outp)
-		lossval.backward()
+        pred = self.model(inp)
+        lossval = self.loss(pred, outp)
+        lossval.backward()
 
-		return (self.model.weight.grad,self.model.bias.grad)
+        return (self.model.weight.grad, self.model.bias.grad)
+
 
 class Agg():
-	def __init__(self,rule):
-		self.rule = rule # rule should be a function that takes a list of gradient updates and aggregates them
+    def __init__(self, rule):
+        self.rule = rule # rule should be a function that takes a list of gradient updates and aggregates them
--- main.py
+++ main.py
@@ -17,52 +17,55 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuff
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
 
+
 def imshow(img):
     img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
+
 model = 
 optim = 
 loss = 
-def rule(ups_list): # ups_list is a tensor
-	return mean(ups_list)[0]
 
-central = Central(model,optim)
+
+def rule(ups_list): # ups_list is a tensor
+    return mean(ups_list)[0]
+
+
+central = Central(model, optim)
 worker_list = []
 for i in range(n_workers):
-	worker_list.append(Worker(loss))
+    worker_list.append(Worker(loss))
 agg = Agg(rule)
 
-e_dist_w = Normal(torch.zeros_like(central.model.weight),mean0_std)
-e_dist_b = Normal(torch.zeros_like(central.model.bias),mean0_std)
-
+e_dist_w = Normal(torch.zeros_like(central.model.weight), mean0_std)
+e_dist_b = Normal(torch.zeros_like(central.model.bias), mean0_std)
 
 
 for t in range(n_epochs):
-	weight_ups = []
-	bias_ups = []
-	for i in range(n_workers):
-		dataiter = iter(trainloader)
-		batch_inp, batch_outp = dataiter.next()
+    weight_ups = []
+    bias_ups = []
+    for i in range(n_workers):
+        dataiter = iter(trainloader)
+        batch_inp, batch_outp = dataiter.next()
 
-		worker_list[i].model = central.model
-		ups = worker_list[i].fwd_bkwd(batch_inp,batch_outp)
-		ups[0] += e_dist_w.sample()
-		ups[1] += e_dist_b.sample()
-		weight_ups.append(ups[0])
-		bias_ups.append(ups[1])
+        worker_list[i].model = central.model
+        ups = worker_list[i].fwd_bkwd(batch_inp, batch_outp)
+        ups[0] += e_dist_w.sample()
+        ups[1] += e_dist_b.sample()
+        weight_ups.append(ups[0])
+        bias_ups.append(ups[1])
 
-	weight_ups_FIN = agg.rule(torch.Tensor(weight_ups)) # aggregate weight grad
-	bias_ups_FIN = agg.rule(torch.Tensor(bias_ups)) # aggregate bias grad
+    weight_ups_FIN = agg.rule(torch.Tensor(weight_ups)) # aggregate weight grad
+    bias_ups_FIN = agg.rule(torch.Tensor(bias_ups)) # aggregate bias grad
 
-	central.update_model((weight_ups_FIN,bias_ups_FIN))
+    central.update_model((weight_ups_FIN, bias_ups_FIN))
 
-	## Evaluate model
+    ## Evaluate model
 
 
 
 
 # TODO: write model & optim & loss
-
