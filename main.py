4f46f3aae366369e36878fed7ebacd02a2ed95d9
grossmanlev <lgrossman@college.harvard.edu>
2019-04-04 16:50:39 -0400

    training working now with FL framework

--- main.py
+++ main.py
@@ -10,24 +10,38 @@ from agents import *
 from models import *
 import copy
 
-n_workers = 20
+n_workers = 2
 n_epochs = 10000
-batch_size = 16
+batch_size = 4
 mean0_std = 0  # 0 if no zero-mean epsilon
-learning_rate = 0.01
+learning_rate = 0.001
 
-transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor(), 
-    torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+transform = torchvision.transforms.Compose(
+    [torchvision.transforms.ToTensor(),
+     torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+# Import Datasets
+trainset = torchvision.datasets.CIFAR10(
+    root='./data', train=True, download=True, transform=transform)
+testset = torchvision.datasets.CIFAR10(
+    root='./data', train=False, download=True, transform=transform)
+
+# Create Validation Split
 valset = copy.deepcopy(trainset)
 trainset.data = trainset.data[0:49000]
 valset.data = valset.data[49000:50000]
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=0)
-valloader = torch.utils.data.DataLoader(valset, batch_size=valset.data.shape[0], shuffle=True, num_workers=0)
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=0)
+# Create Train, Validation, and Test Loaders
+sampler = torch.utils.data.RandomSampler(trainset, replacement=True)
+trainloader = torch.utils.data.DataLoader(
+    trainset, batch_size=batch_size, shuffle=False, sampler=sampler,
+    num_workers=0)
+
+valloader = torch.utils.data.DataLoader(
+    valset, batch_size=valset.data.shape[0], shuffle=True, num_workers=0)
+
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=batch_size, shuffle=False, num_workers=0)
 
 
 def imshow(img):
@@ -37,16 +51,29 @@ def imshow(img):
     plt.show()
 
 
-# Setup Learning Model
-model = SecondNet()
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-loss = nn.CrossEntropyLoss()
-
-
 def rule(ups_list):  # ups_list is a list of list of tensors
     return [torch.stack([x[i] for x in ups_list]).mean(0)
             for i in range(len(ups_list[0]))]
 
+
+def print_test_accuracy(model):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print('Accuracy of the network on the 10000 test images: %d %%' % (
+          100 * correct / total))
+
+
+# Setup Learning Model
+model = SecondNet()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+loss = nn.CrossEntropyLoss()
 
 # Setup Federated Learning Framework
 central = Central(model, optimizer)
@@ -59,11 +86,14 @@ e_dist_w = []
 for layer, paramval in central.model.named_parameters():
     e_dist_w.append(Normal(torch.zeros_like(paramval), mean0_std))
 
+# Training Loop
 for t in range(n_epochs):
     weight_ups = []
     central.model.train()
+    dataiter = iter(trainloader)
+
+    # Worker Loop
     for i in range(n_workers):
-        dataiter = iter(trainloader)
         batch_inp, batch_outp = dataiter.next()
 
         worker_list[i].model = central.model
@@ -74,21 +104,14 @@ for t in range(n_epochs):
 
         weight_ups.append(ups)
 
-    weight_ups_FIN = agg.rule(weight_ups)  # aggregate weight grad
+    # Aggregate Worker Gradients
+    weight_ups_FIN = agg.rule(weight_ups)
 
+    # Update Central Model
     central.update_model(weight_ups_FIN)
 
-    total = 0
-    correct = 0
     central.model.eval()
 
-    if t%500 == 0:
-        for data in valloader:
-            inp, outp = data
-
-            preds = central.model(inp)
-            _, predicted_labs = torch.max(preds, 1)
-            total += outp.size(0)
-            correct += (predicted_labs == outp).sum().item()
-
-        print('Epoch {}, Accuracy of the network on val set: {}'.format(t, 100 * correct / float(total)))
+    if t % 500 == 0:
+        print('Epoch: {}'.format(t))
+        print_test_accuracy(model)
